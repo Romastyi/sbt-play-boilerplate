376b13aeb24926e740548ef8afdcbf7d6747e96d
romastyi <my.beliberda@gmail.com>
2017-06-06 15:53:01 +0300

    Generic parser. Remove SwaggerCodegenPlugin.

--- lib/src/test/scala/play/boilerplate/generators/PrintSyntaxString.scala
+++ lib/src/test/scala/play/boilerplate/generators/PrintSyntaxString.scala
@@ -1,26 +1,12 @@
 package play.boilerplate.generators
 
-import eu.unicredit.swagger.generators.SyntaxString
-
 trait PrintSyntaxString {
 
   def printCodeFile(codeFile: Iterable[CodeFile]): Unit = {
-
     codeFile.foreach { source =>
       println(s"--- ${source.fileName} ---")
       println(source.source)
     }
-
-  }
-
-  def printSyntaxString(ss: Iterable[SyntaxString]): Unit = {
-
-    for (SyntaxString(name, pre, impl) <- ss) yield {
-      println(s"--- $name ---")
-      println(pre)
-      println(impl)
-    }
-
   }
 
   def printRoutes(routes: Seq[String]): Unit = {
