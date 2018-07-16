05e88814842918a37e75eed91a7c9769c3220ec2
romastyi <my.beliberda@gmail.com>
2018-07-16 16:27:18 +0300

    CircuitBreakerId with pattern s"$serviceName.$operationId".

--- api-core/src/main/scala/play/boilerplate/api/client/dsl/CircuitBreakerId.scala
+++ api-core/src/main/scala/play/boilerplate/api/client/dsl/CircuitBreakerId.scala
@@ -1,5 +1,5 @@
 package play.boilerplate.api.client.dsl
 
 final case class CircuitBreakerId(serviceName: String, operationId: String) {
-  def id: String = s"$serviceName-$operationId"
+  def id: String = s"$serviceName.$operationId"
 }
