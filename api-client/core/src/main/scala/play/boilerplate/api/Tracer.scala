c63f9e62699cca1d9989893b1d5cf1cd2c337855
romastyi <romastyi@github.com>
2019-02-16 21:29:23 +0300

    Tracer transformation.

--- api-client/core/src/main/scala/play/boilerplate/api/Tracer.scala
+++ api-client/core/src/main/scala/play/boilerplate/api/Tracer.scala
@@ -3,6 +3,7 @@ package play.boilerplate.api
 trait Tracer {
   def applyMsg(msg: String): String
   def traceId: String
+  def transform(f: String => String): Tracer = Tracer.transform(this)(f)
 }
 
 object Tracer {
@@ -12,10 +13,13 @@ object Tracer {
     override val traceId: String = _traceId
   }
 
-  def empty: Tracer = apply("")
-
   def random(length: Int): Tracer = apply(scala.util.Random.alphanumeric.take(length).mkString(""))
 
   def randomUUID: Tracer = apply(java.util.UUID.randomUUID().toString)
+
+  def transform(t: Tracer)(f: String => String): Tracer = new Tracer {
+    override def applyMsg(msg: String): String = t.applyMsg(f(msg))
+    override def traceId: String = t.traceId
+  }
 
 }
