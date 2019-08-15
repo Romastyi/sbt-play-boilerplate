2120a394aa699fe6beb5a7dd37cae5d459af58b7
romastyi <romastyi@github.com>
2019-08-15 17:36:19 +0300

    Add `tracer` to `RequestHandler`.

--- api-client/share/src/main/scala/play/boilerplate/api/client/dsl/RequestHandler.scala
+++ api-client/share/src/main/scala/play/boilerplate/api/client/dsl/RequestHandler.scala
@@ -1,5 +1,7 @@
 package play.boilerplate.api.client.dsl
 
+import play.boilerplate.api.Tracer
+
 import scala.concurrent.Future
 
 //object Compat extends AbstractCompat
@@ -8,17 +10,17 @@ trait Credentials[+T]
 case object NoCredentials extends Credentials[Nothing]
 
 trait RequestHandler[T] {
-  def beforeRequest(operationId: String, request: Compat.WSRequest, credentials: Credentials[T] = NoCredentials): Future[Compat.WSRequest]
-  def onSuccess(operationId: String, response: Compat.WSResponse, credentials: Credentials[T] = NoCredentials): Unit
-  def onError(operationId: String, cause: Throwable, credentials: Credentials[T] = NoCredentials): Unit
+  def beforeRequest(operationId: String, request: Compat.WSRequest, credentials: Credentials[T] = NoCredentials)(implicit tracer: Tracer): Future[Compat.WSRequest]
+  def onSuccess(operationId: String, response: Compat.WSResponse, credentials: Credentials[T] = NoCredentials)(implicit tracer: Tracer): Unit
+  def onError(operationId: String, cause: Throwable, credentials: Credentials[T] = NoCredentials)(implicit tracer: Tracer): Unit
 }
 
 object RequestHandler {
 
   def default[T]: RequestHandler[T] = new RequestHandler[T] {
-    override def beforeRequest(operationId: String, request: Compat.WSRequest, credentials: Credentials[T]): Future[Compat.WSRequest] = Future.successful(request)
-    override def onSuccess(operationId: String, response: Compat.WSResponse, credentials: Credentials[T]): Unit = ()
-    override def onError(operationId: String, cause: Throwable, credentials: Credentials[T]): Unit = ()
+    override def beforeRequest(operationId: String, request: Compat.WSRequest, credentials: Credentials[T])(implicit tracer: Tracer): Future[Compat.WSRequest] = Future.successful(request)
+    override def onSuccess(operationId: String, response: Compat.WSResponse, credentials: Credentials[T])(implicit tracer: Tracer): Unit = ()
+    override def onError(operationId: String, cause: Throwable, credentials: Credentials[T])(implicit tracer: Tracer): Unit = ()
   }
 
 }
